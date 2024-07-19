16f38970d3d561c4462742d3816c5444bcbb566f
SunshineIDR <sterlingumoren@gmail.com>
2024-07-19 15:43:42 +0100

    Matplotlib

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([5, 2, 1, 4, 8, 4, 10, 15])
-ypoints = np.array([3, 1, 3, 5, 10, 3, 9, 7])
-
+xpoints = np.array([ 2, 2, 13, 13, 2])
+ypoints = np.array([10, 20, 20, 10, 10])
 plt.plot(xpoints, ypoints)
 plt.show()
