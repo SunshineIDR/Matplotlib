89ea43548a95a883912da31e30269534c93d1550
SunshineIDR <sterlingumoren@gmail.com>
2024-10-20 16:28:15 +0100

    Bar Labelling

---
+++ .ipynb_checkpoints/Matplotlib-checkpoint.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([ 2, 2, 13, 13, 2])
+ypoints = np.array([10, 20, 20, 10, 10])
+plt.plot(xpoints, ypoints)
+plt.show()
+#  gita add .
+#  git commit -m "Matplotlib"
+#  git push
